f515befe1064e6ef8abd8a6b2aa3804941df240b
mdwalters <mdwalters.pm@proton.me>
2024-03-10 15:49:46 -0400

    chore: actually make the backend do something

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use rocket::serde::json::*;
+
+#[get("/")]
+fn index() -> Value {
+    json!({
+        "version": "0.1.0"
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount(
+            "/",
+            routes![
+                index
+            ]
+        )
 }
