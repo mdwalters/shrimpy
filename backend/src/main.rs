db557b035f5e4b461fcb348a8bb9f2f8afd70bb4
mdwalters <mdwalters.pm@proton.me>
2024-03-11 01:27:21 -0400

    feat: implement first thing in federation

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,13 +8,24 @@ fn index() -> Value {
     })
 }
 
+#[get("/manifest.json")]
+fn manifest() -> Value {
+    json!({
+        "name": "Shrimpy",
+        "short_name": "Shrimpy",
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#592f28",
+        "theme_color": "#ff401f",
+        "icons": [],
+        "share_target": {}
+    })
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount(
-            "/",
-            routes![
-                index
-            ]
-        )
+    rocket::build().mount("/", routes![
+        index,
+        manifest
+    ])
 }
